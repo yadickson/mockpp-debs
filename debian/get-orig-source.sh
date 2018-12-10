a8cb8a32bc0803ae79894eaabab79909923e439c
Yadickson Soto <yadickson.soto@basedos.cl>
2018-12-10 15:09:26 -0300

    update build project

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -22,14 +22,23 @@ mv tmp/"${PKG}-${VERSION}" "${PKG}-${VERSION}"
 rm -rf tmp
 
 find "${PKG}-${VERSION}" -type f -name '*.cvs' -exec rm -f '{}' \;
-find "${PKG}-${VERSION}" -type f -name '*.sh' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name '*.kdevelop' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name '*.lsm' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name 'Makefile.in' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name 'Makefile' -exec rm -f '{}' \;
-find "${PKG}-${VERSION}" -type f -name '*.m4' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name '*.bat' -exec rm -f '{}' \;
 find "${PKG}-${VERSION}" -type f -name 'CMakeLists.txt' -exec rm -f '{}' \;
+
+find "${PKG}-${VERSION}" -type f -name '*.sh' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name '*.pl' -exec rm -f '{}' \;
+
+find "${PKG}-${VERSION}" -type f -name 'config.guess' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'config.sub' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'depcomp' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'install-sh' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'ltconfig' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'missing' -exec rm -f '{}' \;
+find "${PKG}-${VERSION}" -type f -name 'mkinstalldirs' -exec rm -f '{}' \;
 
 rm -rf "${PKG}-${VERSION}"/3party
 rm -rf "${PKG}-${VERSION}"/bcb5
@@ -38,7 +47,6 @@ rm -rf "${PKG}-${VERSION}"/generator
 rm -rf "${PKG}-${VERSION}"/msvc8
 rm -rf "${PKG}-${VERSION}"/msvc71
 rm -rf "${PKG}-${VERSION}"/tool
-rm -rf "${PKG}-${VERSION}"/config
 rm -rf "${PKG}-${VERSION}"/mockpp/docs
 
 rm -f "${PKG}-${VERSION}"/configure
@@ -53,6 +61,7 @@ rm -f "${PKG}-${VERSION}"/libtool
 rm -f "${PKG}-${VERSION}"/COPYING
 rm -f "${PKG}-${VERSION}"/INSTALL
 rm -f "${PKG}-${VERSION}"/TODO
+rm -f "${PKG}-${VERSION}"/aclocal.m4
 
 if [ "${ADD_PATCH}" != "false" ]
 then
